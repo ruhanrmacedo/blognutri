e2704047399a37a6136ead54b10af88bab21d520
Ruhan <ruhan_350@hotmail.com>
2023-05-03 22:00:05 -0300

    Finalizando...

--- src/main/java/macedos/com/blognutri/entity/Blog.java
+++ src/main/java/macedos/com/blognutri/entity/Blog.java
@@ -1,7 +1,11 @@
 package macedos.com.blognutri.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
--- src/main/resources/db/migration/V2__alterar_colunas_blog.sql
+++ src/main/resources/db/migration/V2__alterar_colunas_blog.sql
@@ -1,4 +1,4 @@
-ALTER TABLE public.blog
+ALTER TABLE IF EXISTS public.blog
     ALTER COLUMN title TYPE varchar(500),
     ALTER COLUMN image TYPE varchar(500),
     ALTER COLUMN description TYPE varchar(2000);
